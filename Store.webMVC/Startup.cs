ef2fc7c4f79c4daeaf2e523952fe0fcb95775901
Jenna Pearmain <ravynskyes88@gmail.com>
2018-08-07 09:50:55 -0400

    Add project files.

---
+++ Store.webMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Store.webMVC.Startup))]
+namespace Store.webMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
